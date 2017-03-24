b9d3637714ec6cb25acbdd53aa88516491ebe172
Niko Uphoff <nikuph@gmail.com>
2017-03-24 17:30:04 +0100

    feat: Add more storybook declartions

--- storybook.js
+++ storybook.js
@@ -1,7 +1,9 @@
 declare module '@kadira/storybook' {
-  declare function storiesOf(): any;
   declare function action(): any;
+  declare function addDecorator(): any;
+  declare function configure(): any;
   declare function linkTo(): any;
+  declare function storiesOf(): any;
 }
 
 declare module '@kadira/storybook-addon-notes' {
