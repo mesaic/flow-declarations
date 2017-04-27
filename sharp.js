7791c9ee52bd33431368a4a7ddd6322bfc125624
Jonathan Werner <jwerner@posteo.de>
2017-04-27 13:40:32 +0200

    make sharp laxer, throws weird errors

--- sharp.js
+++ sharp.js
@@ -1,24 +1,3 @@
-type TMetadata = {
-  format: string,
-  width: number,
-  height: number,
-};
-
 declare module 'sharp' {
-  declare class Sharp extends stream$Duplex {
-    metadata(): Promise<TMetadata>;
-
-    resize(width: ?number, height: ?number): Sharp;
-
-    quality(quality: ?number): Sharp;
-
-    progressive(): Sharp;
-
-    jpeg(): Sharp;
-    png(): Sharp;
-
-    on(event: string, listener: Function): Sharp;
-  }
-
-  declare function exports(): Sharp;
+  declare var exports: any;
 }
