5e2c2e9c3f1da87d14981b86441ef125c66c3fce
Jonathan Werner <jwerner@posteo.de>
2017-05-10 08:32:30 +0200

    Update storybook.js

--- storybook.js
+++ storybook.js
@@ -1,9 +1,6 @@
 declare module '@kadira/storybook' {
-  declare function action(): any;
-  declare function addDecorator(): any;
-  declare function configure(): any;
-  declare function linkTo(): any;
-  declare function storiesOf(): any;
+  declare var exports: any;
+
 }
 
 declare module '@kadira/storybook-addon-notes' {
